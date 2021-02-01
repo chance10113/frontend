482f59221fc9d9d478c6d1be8ae9b465ef0ac84d
ty <tylerpmatthews1998@gmail.com>
2021-02-01 17:29:04 -0500

    test

--- watermyplants/src/Components/Home.js
+++ watermyplants/src/Components/Home.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Home =() => {
     return (
         <div>
-            <h2>Hello, from Home</h2>
+            <h2>Hello, from Home!</h2>
+            <p>I should be hidden if not logged in!</p>
         </div>
     )
 }
