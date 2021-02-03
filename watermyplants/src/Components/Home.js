9cf3a74d474710141aae0d7a2145d761ae52f0d9
Nathan Chagar <nathan.adel.chagar@gmail.com>
2021-02-03 11:20:20 -0800

    Home page form; onchange not yet implemented

--- watermyplants/src/Components/Home.js
+++ watermyplants/src/Components/Home.js
@@ -1,11 +1,92 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import * as yup from 'yup';
+import styled from "styled-components";
+import axios from 'axios';
 
-const Home =() => {
+const schema = yup.object().shape({
+    
+})
+
+const Home = () => {
+
+    const initialFormValues = {
+        id: '',
+        nickName:'',
+        species: '',
+        h2oFrequency: '',
+        image: '',
+    }
+
+
+    const [value, setValue] = useState(initialFormValues);
+    const [disabled, setDisabled] = useState(true);
+
     return (
         <div>
-            <h2>Hello, from Home!</h2>
+            <h1>Pocket Planters</h1>
             <p>I should be hidden if not logged in!</p>
+            <StyledForm onSubmit={onSubmit}>
+            <label>
+                Nickname
+                <input
+                    name="nickName"
+                    type="text"
+                    onChange={onChange}
+                    value={value.nickName}
+                    placeholder="Nickname"
+                />
+            </label>
+            <label>
+                Species
+                <input
+                    name="species"
+                    type="text"
+                    onChange={onChange}
+                    value={value.species}
+                    placeholder="Species"
+                />
+            </label>
+            <label>
+                H2o-Frequency
+                <input
+                    name="h2oFrequency"
+                    type="text"
+                    onChange={onChange}
+                    value={value.h2oFrequency}
+                    placeholder="H2o-Frequency"
+                />
+            </label>
+            <label>
+                Id
+                <input 
+                    name="id"
+                    type="text"
+                    onChange={onChange}
+                    value={value.id}
+                    placeholder="Id"
+                />
+            </label>
+            <label>
+                image
+                <input 
+                    name="image"
+                    type="image"
+                    alt="Plant Photo"
+                    onChange={onChange}
+                    value={value.image}
+                    placeholder="Image: optional"
+                />
+            </label>
+            <button disabled={disabled} className="submit-btn">
+                Add Plant
+            </button>
+            </StyledForm>
         </div>
     )
 }
 export default Home;
+
+const StyledForm = styled.form`
+
+`
